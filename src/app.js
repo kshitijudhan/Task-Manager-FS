05d7951755953844953a1cd7588dc8adc2800e7d
kshitijudhan <udhankshitij@gmail.com>
2026-09-18 12:00:09 +0530

    add crud function

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
+import taskRouter from "./routes/task.routes.js";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/tasks", taskRouter);
 
 export default app;
---
+++ src/controllers/task.controller.js
@@ -0,0 +1,70 @@
+import { Task } from "../models/task.model.js";
+
+export const createTask = async (req, res) => {
+  const { title, description } = req.body;
+
+  const task = await Task.create({
+    title,
+    description,
+  });
+
+  res.status(201).json({
+    success: true,
+    message: "Task created suyccessfully",
+    task,
+  });
+};
+
+export const getTasks = async (req, res) => {
+  const tasks = await Task.find();
+
+  res.status(200).json({
+    success: true,
+    tasks,
+  });
+};
+
+export const getTaskById = async (req, res) => {
+  const id = req.params.id;
+
+  const task = await Task.findById(id);
+
+  res.status(200).json({
+    success: true,
+    task,
+  });
+};
+
+export const updateTask = async (req, res) => {
+  const { id } = req.params;
+  const { title, description, completed } = req.body;
+
+  const task = await Task.findByIdAndUpdate(
+    id,
+    {
+      title,
+      description,
+      completed,
+    },
+    {
+      new: true,
+    },
+  );
+
+  res.status(200).json({
+    success: true,
+    task,
+  });
+};
+
+export const deleteTask = async (req, res) => {
+  const { id } = req.params;
+
+  const task = await Task.findByIdAndDelete(id);
+
+  res.status(200).json({
+    success: true,
+    message: "Task Deleted",
+    task,
+  });
+};
